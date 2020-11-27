2a77b58aa472eef2e6c2247c4e6ccaeb45f2dd86
siddhantj048 <63706698+siddhantj048@users.noreply.github.com>
2020-11-27 16:05:46 +0530

    Update ChatHeader.js

--- discord/src/ChatHeader.js
+++ discord/src/ChatHeader.js
@@ -1,47 +1,33 @@
 import React from 'react';
 import './ChatHeader.css';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import EditLocationRoundedIcon from '@material-ui/icons/EditLocationRounded';
 import PeopleAltRoundedIcon from '@material-ui/icons/PeopleAltRounded';
+import WeekendIcon from '@material-ui/icons/Weekend';
 import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
 import SendRoundedIcon from '@material-ui/icons/SendRounded';
-import HelpRoundedIcon from '@material-ui/icons/HelpRounded';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import MoodIcon from '@material-ui/icons/Mood';
+function ChatHeader({channelName}) {
+    return (
+        <div className="chatHeader">
+            <div className="chatHeader_left">
+                <h3>
+                    <span className="chatHeader_hash">#</span>{channelName}
+                </h3>
+            </div>
 
-function ChatHeader({ channelName }) {
-    return ( <
-        div className = "chatHeader" >
-        <
-        div className = "chatHeader_left" >
-        <
-        h3 >
-        <
-        span className = "chatHeader_hash" > # < /span>{channelName} <
-        /h3> <
-        /div>
+            <div className="chatHeader_right">
+                <WhatshotIcon fontSize="large" />
+                <MoodIcon fontSize="large"/>
+                <PeopleAltRoundedIcon fontSize="large" />
 
-        <
-        div className = "chatHeader_right" >
-        <
-        NotificationsIcon / >
-        <
-        EditLocationRoundedIcon / >
-        <
-        PeopleAltRoundedIcon / >
-
-        <
-        div className = "chatHeader_search" >
-        <
-        input placeholder = "Search" / >
-        <
-        SearchRoundedIcon / >
-        <
-        /div> <
-        SendRoundedIcon / >
-        <
-        HelpRoundedIcon / >
-        <
-        /div> <
-        /div>
+                <div className="chatHeader_search">
+                    <input placeholder="Search" />
+                    <SearchRoundedIcon />
+                </div>
+                <SendRoundedIcon fontSize="large" />
+                <WeekendIcon fontSize="large" />
+            </div>
+        </div>
     )
 }
 
