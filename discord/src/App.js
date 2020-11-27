d67481c6df1b190625c7e3ca97bdc270167cdf01
siddhantj048 <63706698+siddhantj048@users.noreply.github.com>
2020-11-27 16:00:53 +0530

    Update App.js

--- discord/src/App.js
+++ discord/src/App.js
@@ -1,52 +1,53 @@
-import React, { useEffect } from 'react';
+import React , {useEffect} from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
 import { useSelector, useDispatch } from "react-redux";
-import { login, selectUser, logout } from './features/userSlice';
+import { login, selectUser,logout } from './features/userSlice';
 import Login from './Login';
-import { auth } from './firebase';
+import {auth} from './firebase';
 
 
 
 
 function App() {
-    const dispatch = useDispatch();
-    const user = useSelector(selectUser);
+  const dispatch =  useDispatch();
+  const user = useSelector(selectUser);
 
-    useEffect(() => {
-        auth.onAuthStateChanged((authUser) => {
-            if (authUser) {
-                //usdhuij
-                dispatch(login({
-                    uid: authUser.uid,
-                    photo: authUser.photoURL,
-                    email: authUser.email,
-                    displayName: authUser.displayName,
-                }))
-            } else {
-                dispatch(logout());
-            }
-        });
-    }, [dispatch]);
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if(authUser) {
+        //usdhuij
+        dispatch(login({
+          uid:authUser.uid,
+          photo:authUser.photoURL,
+          email:authUser.email,
+          displayName:authUser.displayName,
+        }))
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
 
-    return ( <
-        div className = "app" >
-        <
-        div className = "app_body" > {
-            user ? ( <
-                > { /*Sidebar*/ } <
-                Sidebar / > { /*chat*/ } <
-                Chat / >
-                <
-                />
-            ) : ( <
-                Login / >
-            )
-        } <
-        /div> <
-        /div>
-    );
+  return (
+    <div className="app">
+         
+      <div className="app_body">
+     
+    {user? (
+      <>
+      {/*Sidebar*/}
+      <Sidebar />
+      {/*chat*/}
+      <Chat />
+      </>
+    ) : (
+       <Login />
+    )}
+    </div>
+    </div>
+  );
 }
 
 export default App;
