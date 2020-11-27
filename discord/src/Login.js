d6f4a09046c028a4be3725168a9e7d01e6882443
siddhantj048 <63706698+siddhantj048@users.noreply.github.com>
2020-11-27 16:08:05 +0530

    Update Login.js

--- discord/src/Login.js
+++ discord/src/Login.js
@@ -1,25 +1,23 @@
+
 import React from 'react';
 import './Login.css';
-import { auth, provider } from './firebase';
+import {auth, provider} from './firebase';
 
 function Login() {
-    const signIn = () => {
+    const signIn = () =>{
         //idek
         auth.signInWithPopup(provider)
-            .catch((error) => alert(error.message));
+        .catch((error) => alert(error.message) );
     };
-    return ( <
-        div className = "login" >
-        <
-        div className = "login_logo" >
-        <
-        img src = "https://th.bing.com/th/id/OIP.zDYsd2MiyCgU-Qsb0mLCYAHaHa?pid=Api&rs=1"
-        alt = "" / >
-        <
-        /div> <
-        button onClick = { signIn } > Sign In < /button> <
-        /div>
+    return (
+        <div className="login">  
+            <div className="login_logo">
+                <img className="hvr-bounce-in" src="https://th.bing.com/th/id/OIP.zDYsd2MiyCgU-Qsb0mLCYAHaHa?pid=Api&rs=1"
+                alt="" />
+            </div>
+            <button onClick={signIn}>Sign in with Google</button>
+        </div>
     );
 }
 
-export default Login
+export default Login;
