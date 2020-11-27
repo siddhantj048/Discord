d95b247cc745640dcdd1d26fb2fbd72d3737cfd0
siddhantj048 <63706698+siddhantj048@users.noreply.github.com>
2020-11-27 16:12:14 +0530

    Update SidebarChannel.js

--- discord/src/SidebarChannel.js
+++ discord/src/SidebarChannel.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import './SidebarChannel.css';
-import { setChannelInfo } from "./features/appSlice";
+import {setChannelInfo} from "./features/appSlice";
 
-export default function SidebarChannel({ id, channelName }) {
+export default function SidebarChannel({id , channelName}) {
     const dispatch = useDispatch();
-    return ( <
-        div className = "sidebarChannel"
-        onClick = {
-            () => dispatch(setChannelInfo({
-                channelId: id,
-                channelName: channelName,
-            }))
-        } >
-        <
-        h4 >
-        <
-        span className = "sidebarChannel_hash" > # < /span>{channelName} <
-        /h4>
-
-        <
-        /div>
+    return (
+        <div className="sidebarChannel" onClick={ () => dispatch(setChannelInfo({
+            channelId:id,
+            channelName:channelName,
+        }))}>
+            <h4>
+                <span className="sidebarChannel_hash">#</span>{channelName}
+            </h4>
+            
+        </div>
     )
 }
