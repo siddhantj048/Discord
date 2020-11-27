371dc05e66d440589e9f4e2ffa547869b425996e
siddhantj048 <63706698+siddhantj048@users.noreply.github.com>
2020-11-27 16:09:04 +0530

    Update Message.js

--- discord/src/Message.js
+++ discord/src/Message.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import './Message.css'
-import { Avatar } from '@material-ui/core';
+import {Avatar} from '@material-ui/core';
 
-function Message({ timestamp, user, message }) {
-    return ( <
-        div className = "message" >
-        <
-        Avatar src = { user.photo }
-        /> <
-        div className = "message_info" >
-        <
-        h4 > { user.displayName } <
-        span className = "message_timestamp" > { new Date().toUTCString() } <
-        /span> <
-        /h4>
+function Message({timestamp,user,message}) {
+    return (
+        <div className="message">
+            <Avatar src={user.photo} />
+            <div className="message_info">
+                <h4>
+                    {user.displayName}
+                    <span className="message_timestamp">
+                      {new Date().toUTCString()}
+                    </span>
+                </h4>
 
-        <
-        p > { message } < /p> <
-        /div> <
-        /div>
+                <p>{message}</p>
+            </div>
+        </div>
     )
 }
 
